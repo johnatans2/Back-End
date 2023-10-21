73f31f7b0fd0ec7b438716e3d9a2ed208b9431f6
johnatans2 <johnatan.henrique14@hotmail.com>
2023-10-21 19:19:31 -0300

    Finalizando Projeto

--- Projetos DIO/Lógica de Programação/Desafio do Herói.js
+++ Projetos DIO/Lógica de Programação/Desafio do Herói.js
@@ -1,1 +1,23 @@
-console.log("Hello World!")
+//Declaração de Variáveis e Inputs
+const Hero=["nome","XP"];
+let Ranking;
+//Processamento de Dados
+if(Hero[1]<1000){
+    Ranking="Ferro";
+}else if(Hero[1]>1000&&Hero[1]<=2000){
+    Ranking="Bronze";
+}else if(Hero[1]>2000&&Hero[1]<=5000){
+    Ranking="Prata";
+}else if(Hero[1]>5000&&Hero[1]<=7000){
+    Ranking="Ouro";
+}else if(Hero[1]>7000&&Hero[1]<=8000){
+    Ranking="Platina";
+}else if(Hero[1]>8000&&Hero[1]<=9000){
+    Ranking="Ascendente";
+}else if(Hero[1]>9000&&Hero[1]<=10000){
+    Ranking="Imortal";
+}else{
+    Ranking="Radiante";
+}
+//Output
+console.log(`O herói de nome ${Hero[0]}, de nível ${Hero[1]}, está no Ranking ${Ranking}.`.toUpperCase());
